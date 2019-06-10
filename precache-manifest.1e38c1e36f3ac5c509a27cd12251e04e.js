9888f95fbcc68fc788f742def186d7610623d156
BartBros <bb.aambroziak@gmail.com>
2019-06-10 10:42:11 +0200

    Updates

---
+++ precache-manifest.1e38c1e36f3ac5c509a27cd12251e04e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f93c6cd83f4c0d390b0b8ccd46ba6b86",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8872e084ba0c2437efd1",
+    "url": "/static/css/main.bd21fda2.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "8872e084ba0c2437efd1",
+    "url": "/static/js/main.2cc46dc5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
